04b275a008f1587f885c6f9727ae555ef8605c27
wonglok <yellowhappy831@gmail.com>
2020-03-21 06:27:14 +0800

    github backup

---
+++ vue.config.js
@@ -0,0 +1,29 @@
+if (process.env.NODE_ENV === 'development') {
+  require('./src/human/vue-entrty')
+}
+
+module.exports = {
+  devServer: {
+    // proxy: {
+    //   // netlify dev with functions
+    //   '/.netlify/functions/': {
+    //     target: 'http://localhost:8888'
+    //   }
+    // }
+    // proxy: 'http://tunnel.wonglok.com:8080'
+  },
+  css: {
+    extract: false
+  },
+  configureWebpack: {
+    plugins: [
+      // new MonacoWebpackPlugin()
+    ]
+  },
+  pages: {
+    index: 'src/main.js'
+    // runner: {
+    //   entry: 'src/runner.js'
+    // }
+  }
+}
